358e01f4830199ec7824f92f55c1984fc870cf66
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-12-31 03:29:20 +0500

    fix issue

---
+++ eslintrc.ts
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // Disable 'any' rule
+      'react/no-unescaped-entities': 'off', // Disable unescaped entities rule
+      'import/no-anonymous-default-export': 'off', // Disable anonymous export rule
+    },
+  };
+  
