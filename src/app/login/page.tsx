2fa038a0ce11831df43c51bd051573d82af1a6b0
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-01-01 16:45:45 +0500

    login content aligned center

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 function Login() {
   return (
     <div>
-        <main className="container mt-8 mx-auto p-4">
-        <h1 className="text-center block text-3xl font-bold mb-4">Login</h1>
+        <main className="container flex flex-col justify-center items-center mt-8 mx-auto p-4">
+        <h1 className="block text-3xl font-bold mb-4">Login</h1>
         <SignedOut>
               <SignIn routing="hash"/>
             </SignedOut>
